28f51faf260b4223c3182d2faf6bfa89940a2ea0
Stefan Erhard <erhard.stefan@vareal.co.jp>
2018-02-27 15:11:47 +0900

    support number only tags

--- lib/simple_hashtag/hashtag.rb
+++ lib/simple_hashtag/hashtag.rb
@@ -11,7 +11,7 @@ module SimpleHashtag
     # TODO: Beef up the regex (ie.:what if content is HTML)
     # this is how Twitter does it:
     # https://github.com/twitter/twitter-text-rb/blob/master/lib/twitter-text/regex.rb
-    HASHTAG_REGEX = /(?:\s|　|^)([#|＃|#](?!(?:\d+|\w+?_|_\w+?)(?:\s|$))([一-龯０-９Ａ-ｚｧ-ﾝﾞﾟぁ-んァ-ンa-z0-9\-_]+))/i
+    HASHTAG_REGEX = /(?:\s|　|^)([#|＃|#]([一-龯０-９Ａ-ｚｧ-ﾝﾞﾟぁ-んァ-ンa-z0-9\-_]+))/i
 
     def self.find_by_name(name)
       Hashtag.where('lower(name) =?', name.downcase).first
