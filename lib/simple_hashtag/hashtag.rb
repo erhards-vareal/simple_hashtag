bcc9d70ec495d14724957f78f7e2e468b996c772
Stefan Erhard <erhard.stefan@vareal.co.jp>
2018-03-23 16:34:24 +0900

    ignore space in front of hashtag

--- lib/simple_hashtag/hashtag.rb
+++ lib/simple_hashtag/hashtag.rb
@@ -11,7 +11,7 @@ module SimpleHashtag
     # TODO: Beef up the regex (ie.:what if content is HTML)
     # this is how Twitter does it:
     # https://github.com/twitter/twitter-text-rb/blob/master/lib/twitter-text/regex.rb
-    HASHTAG_REGEX = /(?:\s|　|^)([#]([ゝヽゞヾ〱〲々〃ー−０-９ｦ-ﾟァ-ヶぁ-ゞＡ-ｚ一-龯a-z0-9\-_]+))/i
+    HASHTAG_REGEX = /(?:\s|^)([#]([ゝヽゞヾ〱〲々〃ー−０-９ｦ-ﾟァ-ヶぁ-ゞＡ-ｚ一-龯a-z0-9\-_]+))/i
 
     def self.find_by_name(name)
       Hashtag.where('lower(name) =?', name.downcase).first
